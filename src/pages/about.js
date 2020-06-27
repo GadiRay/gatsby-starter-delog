ae4e609ea86ee889982e05060063097370f398c4
Gadi Raymond <gadi.r@walkme.com>
2020-06-27 17:09:21 +0300

    wip

---
+++ src/pages/about.js
@@ -0,0 +1,32 @@
+import React from "react"
+import Helmet from "react-helmet"
+import { graphql } from 'gatsby'
+import Layout from "../components/layout"
+
+const AboutPage = ({
+  data: {
+    site
+  },
+}) => {
+  return (
+    <Layout>
+      <Helmet>
+        <title>Contact — {site.siteMetadata.title}</title>
+        <meta name="description" content={site.siteMetadata.description} />
+      </Helmet>
+      
+
+    </Layout>
+  )
+}
+export default AboutPage
+export const pageQuery = graphql`
+  query AboutPageQuery{
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
